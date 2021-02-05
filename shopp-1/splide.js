f44db8c63d092eec6af04a95294c486f8838d311
Sonia <76215726+code-above@users.noreply.github.com>
2021-02-05 15:58:10 -0500

    add splide.js

--- shopp-1/splide.js
+++ shopp-1/splide.js
@@ -22,9 +22,6 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	} ).mount();
 	
 
-
-
-
 	var primarySlider = new Splide( '#primary-slider', {
 		type       : 'fade',
 		heightRatio: 0.5,
@@ -35,6 +32,52 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	} ); // do not call mount() here.
 	
 	primarySlider.sync( secondarySlider ).mount();
+
+
+
+document.querySelectorAll('.primary-01').forEach(carousel => new Splide( carousel, {
+  type       : 'fade',
+		heightRatio: 0.5,
+		pagination : false,
+		arrows     : false,
+		cover      : false,
+}).mount());
+
+document.querySelectorAll('.secondary-01').forEach(carousel => new Splide( carousel, {
+  fixedWidth  : 80,
+		fixedHeight : 80,
+		width       : 290,
+		gap         : 10,
+		cover       : false,
+		isNavigation: true,
+		autoWidth   : false,
+        rewind      : true,
+        pagination  : false,
+		focus       : 'center',
+		breakpoints : {
+			'600': {
+				fixedWidth: 66,
+				height    : 40,
+			}
+		},
+}).mount());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 } );
 
 
