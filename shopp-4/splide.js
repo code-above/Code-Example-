144204e0505d276e49c6be454ea977f171981074
Sonia <76215726+code-above@users.noreply.github.com>
2021-02-24 11:22:39 -0500

    add mobile

--- shopp-4/splide.js
+++ shopp-4/splide.js
@@ -1,45 +1,29 @@
+
 document.addEventListener( 'DOMContentLoaded', function () {
-	var secondarySlider = new Splide( '#secondary-slider', {
-		fixedWidth  : 100,
-		height      : 60,
+    
+          
+document.querySelectorAll('.primary-01').forEach(carousel => new Splide( carousel, {
+        type       : 'fade',
+		heightRatio: 0.5,
+		pagination : false,
+		cover      : true,
+        rewind     : true, 
+        fixedHeight: 600,
+        width:     750,
+        arrows:     false,
+       
+        
+    
+}).mount());
+
+document.querySelectorAll('.secondary-01').forEach(carousel => new Splide( carousel, {
+  fixedWidth  : 100,
+		fixedHeight : 100,
+		width       : 750,
 		gap         : 10,
 		cover       : true,
 		isNavigation: true,
-		focus       : 'center',
-		breakpoints : {
-			'600': {
-				fixedWidth: 66,
-				height    : 40,
-			}
-		},
-	} ).mount();
-	
-	var primarySlider = new Splide( '#primary-slider', {
-		type       : 'fade',
-		heightRatio: 0.5,
-		pagination : false,
-		arrows     : false,
-		cover      : true,
-	} ); // do not call mount() here.
-	
-	primarySlider.sync( secondarySlider ).mount();
-
-
-    document.querySelectorAll('.primary').forEach(carousel => new Splide( carousel, {
-  type       : 'fade',
-		heightRatio: 0.5,
-		pagination : false,
-		arrows     : false,
-		cover      : false,
-}).mount());
-
-document.querySelectorAll('.secondary').forEach(carousel => new Splide( carousel, {
-  fixedWidth  : 80,
-		fixedHeight : 80,
-		width       : 290,
-		gap         : 10,
-		cover       : false,
-		isNavigation: true,
+        arrows      : true,
 		autoWidth   : false,
         rewind      : true,
         pagination  : false,
@@ -53,5 +37,6 @@ document.querySelectorAll('.secondary').forEach(carousel => new Splide( carousel
 }).mount());
 
 
-} );
 
+
+} );
