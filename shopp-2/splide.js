c3a053ea6e9f6f45a291dce55c2a7478715cb862
Sonia <76215726+code-above@users.noreply.github.com>
2021-03-22 09:16:25 -0400

    add clothing  theme

--- shopp-2/splide.js
+++ shopp-2/splide.js
@@ -1,8 +1,8 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 	var secondarySlider = new Splide( '#secondary-slider', {
-		fixedWidth  : 80,
-		height      : 200,
-		gap         : 20,
+		fixedWidth  : 100,
+		height      : 400,
+		gap         : 10,
         direction  : 'ttb',
 	    heightRatio: 0.2,
 	    perPage    : 3,
@@ -30,7 +30,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
 		cover      : true,
         fixedWidth : 330,
         width      : 400,
-        height     : 300,
+        height     : 500,
 	} ); // do not call mount() here.
 	
 	primarySlider.sync( secondarySlider ).mount();
