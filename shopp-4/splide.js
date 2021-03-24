68ca9c7000eb91647f754b96e9ad918f76205868
Sonia <76215726+code-above@users.noreply.github.com>
2021-03-24 14:14:53 -0400

    fix splide

--- shopp-4/splide.js
+++ shopp-4/splide.js
@@ -1,50 +1,52 @@
 
-// document.addEventListener( 'DOMContentLoaded', function () {
- 
-	
-
-// document.querySelectorAll('.secondary').forEach(carousel => new Splide( carousel, {
-//   fixedWidth  : 100,
-// 		fixedHeight : 100,
-// 		width       : 500,
-// 		gap         : 10,
-// 		cover       : true,
-// 		isNavigation: true,
-//         arrows      : true,
-// 		autoWidth   : false,
-//         rewind      : true,
-//         pagination  : false,
-// 		focus       : 'center',
-// 		breakpoints : {
-// 			'600': {
-// 				fixedWidth: 66,
-// 				height    : 40,
-// 			}
-// 		},
-
-// }).mount());
-
-// document.querySelectorAll('.primary').forEach(carousel => new Splide( carousel, {
-//         type       : 'fade',
-// 		heightRatio: 0.5,
-// 		pagination : false,
-// 		cover      : true,
-//         rewind     : true, 
-//         fixedHeight: 500,
-//         width:     450,
-//         arrows:     false,
-		
-
-
-
-// }).mount());
-
-
-// } );
 
 document.addEventListener( 'DOMContentLoaded', function () {
+ 
 
-	var secondarySlider = new Splide( '#second-slider', {
+ 
+document.querySelectorAll('.secondary').forEach(carousel => new Splide( carousel, {
+  fixedWidth  : 100,
+		fixedHeight : 100,
+		width       : 500,
+		gap         : 10,
+		cover       : true,
+		isNavigation: true,
+        arrows      : true,
+		autoWidth   : false,
+        rewind      : true,
+        pagination  : false,
+		focus       : 'center',
+		breakpoints : {
+			'600': {
+				fixedWidth: 66,
+				height    : 40,
+			}
+		},
+
+
+
+}).mount());
+
+document.querySelectorAll('.primary').forEach(carousel => new Splide( carousel, {
+        type       : 'fade',
+		heightRatio: 0.5,
+		pagination : false,
+		cover      : true,
+        rewind     : true, 
+        fixedHeight: 500,
+        width:     450,
+        arrows:     false,
+	
+
+}).mount());
+
+
+
+
+} );
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	var sliderOne = new Splide( '#slider-one', {
 		fixedWidth  : 100,
 		height      : 60,
 		gap         : 10,
@@ -58,9 +60,8 @@ document.addEventListener( 'DOMContentLoaded', function () {
 			}
 		},
 	} ).mount();
-
-
-	var primarySlider = new Splide( '#prim-slider', {
+	
+	var sliderTwo = new Splide( '#slider-two', {
 		type       : 'fade',
 		heightRatio: 0.5,
 		pagination : false,
@@ -68,5 +69,5 @@ document.addEventListener( 'DOMContentLoaded', function () {
 		cover      : true,
 	} ); // do not call mount() here.
 	
-	primarySlider.sync( secondarySlider ).mount();
+	sliderOne.sync( sliderTwo ).mount();
 } );
